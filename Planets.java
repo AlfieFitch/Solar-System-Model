9b5fa73a9ea172b4f32100a1f1b342d6c3be002f
Alfie Fitch-May <alfiefitch0@gmail.com>
2022-12-07 15:53:08 +0000

    Comments!

--- Planets.java
+++ Planets.java
@@ -1,4 +1,8 @@
+// The planets class creates each specfic planet depending on the parameters.
+
 public class Planets{
+
+    //Variable Initialisation:
 
     private String name;
     private double distance;
@@ -6,12 +10,15 @@ public class Planets{
     private double gravity = 0;
     private double mass;
     private double luminosityFactor;
+    // used in the rouding formula as the bound;
     private static final double ROUNDING_VARIABLE = 1000.0;
     private static final double DISTANCE_MIN = 0.75;
     private static final double DISTANCE_MAX = 2.0;
     private static final double MASS_MIN = 0.6;
     private static final double MASS_MAX = 7.0;
 
+    
+    // Constructor used for levels 1 and 2 where other parameters are not used;
     public Planets(String name, double distance){
         this.name = name;
         this.distance = distance;
@@ -19,6 +26,8 @@ public class Planets{
         period = Math.round(periodUnrounded*ROUNDING_VARIABLE)/ROUNDING_VARIABLE;
     }
 
+
+    // Constructor used for level 3 where additional parameters are used.
     public Planets(String name, double mass, double radius, double distance, double starLuminosity){
         this.name = name;
         this.mass = mass;
@@ -28,6 +37,8 @@ public class Planets{
         double periodUnrounded = Math.sqrt(distance*distance*distance)*ROUNDING_VARIABLE/ROUNDING_VARIABLE;
         period = Math.round(periodUnrounded*ROUNDING_VARIABLE)/ROUNDING_VARIABLE;
     }
+
+    // Methods below return the private variable to allow the data to be retrieved.
 
     public String getName(){
         return name;
@@ -49,6 +60,8 @@ public class Planets{
         return gravity;
     }
 
+
+    // Calculates if the planet is habitable, returning yes if it is and no if not.
     public String getHabitable(){
         if (((DISTANCE_MIN * luminosityFactor) <= distance
         && distance <= (DISTANCE_MAX * luminosityFactor)) && ((MASS_MIN <= mass) && (mass <= MASS_MAX))){
@@ -57,6 +70,8 @@ public class Planets{
         return "no";
     }
 
+    // Checks to see if the planet in the passed parameter is equal to the current planet,
+    // depending on the name and distance of the planets.
     public boolean equals(Planets planet) {
         if (planet.getName() == this.name && planet.getDistance() == this.distance){
             return true;
--- SolarSystem.java
+++ SolarSystem.java
@@ -1,37 +1,60 @@
 import java.util.ArrayList;
+
+// The Solar system class creates and stores each planet that is added to the solar system object.
 public class SolarSystem {
 
+    // Variable initalisation.
     private String name;
     private  double luminosity;
+    // The Array below is used to store all the planet objects.
     private ArrayList<Planets> planets = new ArrayList<Planets>();
 
+    // The constructor used for level 3, when luminosity is passed;
     public SolarSystem(String name, double luminosity){
         this.name = name;
         this.luminosity = luminosity;
     }
 
+
+    // The constructor used for level 1 and 2.
     public SolarSystem(String name){
         this.name = name;
     }
 
+
+    // Method that creates a new planet object with the parameters passed to it.
+    // Adds the new planet object to the solar systems Array.
+    // This method is only used in level 1 and 2.
     public void addPlanet(String pName, double distance){
         Planets planet = new Planets(pName, distance);
         planets.add(planet);
     }
 
+
+    // This method is similar to the method above but is used in level 3 when
+    // more parameters are passed to the method and object.
     public void addPlanet(String pName, double mass, double radius, double distance){
         Planets planet = new Planets(pName, mass, radius, distance, luminosity);
         planets.add(planet);
     }
 
+
+    // This method creates the string that is printed out, showing all the planets in the class;
     public String toString(){
+        // The string that shows the star name:
         String finalstring = "Star " + this.name + " has planets:\n";
+        
+        // Loops through every planet in the Array.
         for (int i = 0; i < planets.size(); i++){
+            // the below section is run if the planets gravity is not set,
+            // so this is for level 1 and 2;
             if (planets.get(i).getGravity() == 0){
                 finalstring = finalstring + planets.get(i).getName() 
                 + "  is " + planets.get(i).getDistance() 
                 + "AU from its star, and orbits in " 
                 + planets.get(i).getPeriod() + " years\n";
+            // The else section is ran when there is a value that is set for gravity,
+            // so this is ran for level 3, changing the
             }else {
                 finalstring = finalstring + planets.get(i).getName() + " has a mass of " + planets.get(i).getMass()
                 + " Earths with a surface gravity of " 
@@ -46,8 +69,10 @@ public class SolarSystem {
     }
 
 
+    // The below method gets the from the object by name;
     public Planets getPlanetByName(String planetName){
         Planets sendPlanet = null;
+        // Loops through the Array and checks if the name is equal to that passed by the parameter.
         for (int i = 0; i < planets.size(); i++){
             if (planets.get(i).getName().equals(planetName)){
                 sendPlanet = planets.get(i);
@@ -56,11 +81,15 @@ public class SolarSystem {
         return sendPlanet;
     }
 
+
+    // Gets the planet from the array which is the furthest distance from the sun.
     public Planets furthest(){
         Planets planet = null;
         double tempFurthestDistance = 0;
         for (int i = 0; i < planets.size(); i++){
+            // Compares if the new planet distance is greater than the current one. 
             if (planets.get(i).getDistance() > tempFurthestDistance){
+                // Sets the new planet as furthest if true.
                 tempFurthestDistance = planets.get(i).getDistance();
                 planet = planets.get(i);
             }
@@ -68,11 +97,15 @@ public class SolarSystem {
         return planet;
     }
 
+
+    // Gets the planet from the array which is the closest distance from the sun.
     public Planets closest(){
         Planets planet = null;
         double tempClosestDistance = 0;
         for (int i = 0; i < planets.size(); i++){
+            // Compares if the new planets distance is closer than the current one;
             if ((planets.get(i).getDistance() < tempClosestDistance) || tempClosestDistance == 0){
+                // Sets the new planet as the closest if true, or closest distance is equal to 0;
                 tempClosestDistance = planets.get(i).getDistance();
                 planet = planets.get(i);
             }
